56a23d942b3086d546d23db7163323b6ecbdd47d
dey-subhajit <subhajit.dey1998@yahoo.com>
2021-08-24 20:01:00 +0530

    First commit

---
+++ app/src/main/java/com/example/firebase/Employee.java
@@ -0,0 +1,40 @@
+package com.example.firebase;
+
+public class Employee {
+    String name;
+    String phone_number;
+    String address;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String phone_number, String address) {
+        this.name = name;
+        this.phone_number = phone_number;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.firebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText et_name, et_phone_no, et_address;
+    Button btn_save;
+    FirebaseDatabase fd;
+    DatabaseReference dr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et_name = findViewById(R.id.et_name);
+        et_phone_no = findViewById(R.id.et_phone_no);
+        et_address = findViewById(R.id.et_address);
+        btn_save = findViewById(R.id.btn_save);
+
+        fd = FirebaseDatabase.getInstance();
+        dr = fd.getReference("Employee");
+
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name, phone, address;
+                name = et_name.getText().toString();
+                phone = et_phone_no.getText().toString();
+                address = et_address.getText().toString();
+                createNewData(name, phone, address);
+            }
+        });
+    }
+    public void createNewData(String name, String phone, String address){
+        dr.child(phone).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                if(snapshot.getValue() != null){
+                    Snackbar.make(btn_save, "Data already inserted", Snackbar.LENGTH_LONG).show();
+                }
+                else{
+                    dr.child(phone).setValue(new Employee(name, phone, address));
+                    Snackbar.make(btn_save, "Insert successfully", Snackbar.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Snackbar.make(btn_save, "Server Error", Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+}
